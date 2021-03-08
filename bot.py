9d5818eb209959a6c84967e503d0b177468dc5fb
Kai Tao <kai.tao@citrix.com>
2021-03-08 18:26:03 +0800

    s

--- bot.py
+++ bot.py
@@ -3,14 +3,21 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path= env_path)
 
-app = Flask(__name__)
+SLACK_SIGNING_SECRET = os.environ['SLACK_SIGNING_SECRET']
+SLACK_TOKEN= os.environ['SLACK_TOKEN']
 
-client = slack.WebClient(token = os.environ['SLACK_TOKEN'])
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(
+    SLACK_SIGNING_SECRET,'/slack/events',app
+)
+
+client = slack.WebClient(token = SLACK_TOKEN)
 client.chat_postMessage(channel = '#bot-test-kai', text = "Hello world.")
 
 if __name__ == "__main__":
-    app.run(debug = True, port = 5000)
+    app.run(debug = True, port = 80)
